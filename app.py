5069666d400d777f79f52232dcce723e240c0cfb
Hart Robinson <hart1217d@gmail.com>
2024-07-14 13:12:03 -0500

    Added form and button to login.html Added app.route for login post and get Created database tsranker.db

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 import os
 
+import sqlite3
 # need a module that will serve to update SQL databases (from cs50 import SQL)
 from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import login_required
 
+# Configure application
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+con = sqlite3.connect("tsranker.db")
+cur = con.cursor()
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/")
 @login_required
@@ -13,3 +32,40 @@ def index():
     user_id = session["user_id"]
     "show history"
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("login.html")
+        
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("login.html")
+        
+        # Query database for username
+        rows = cur.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username") 
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return render_template("login.html")
+        
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+    
+    else:
+        return render_template("login.html")
