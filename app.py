309a127d4e5d2754ff563471da2f7776aa51123a
Hart Robinson <hart1217d@gmail.com>
2024-07-14 20:02:47 -0500

    Created register page and registered for testing Added to layout.html

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-con = sqlite3.connect("tsranker.db")
+con = sqlite3.connect('C:/Users/Hart/OneDrive/Documents/Coding/CS50/FinalProject/tsranker.db', check_same_thread=False)
 cur = con.cursor()
 
 @app.after_request
@@ -51,20 +51,57 @@ def login():
         
         # Query database for username
         rows = cur.execute(
-            "SELECT * FROM users WHERE username = ?", request.form.get("username") 
+            "SELECT * FROM users WHERE username = ?", [request.form.get("username")] 
         )
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(
-            rows[0]["hash"], request.form.get("password")
+        if rows.fetchone() is None or not check_password_hash(
+            rows[2], request.form.get("password")
         ):
             return render_template("login.html")
         
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = id
 
         # Redirect user to home page
         return redirect("/")
     
     else:
         return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Change User's Password"""
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username or not password or not confirmation:
+            return render_template("register.html")
+        
+        if not password == confirmation:
+            return render_template("register.html")
+        
+        try:
+            hash = generate_password_hash(password, salt_length=16)
+            data = (username, hash)
+            cur.execute("INSERT INTO users (username, hash) VALUES (?, ?)", data)
+            con.commit()
+            return render_template("login.html")
+        except ValueError:
+            return render_template("register.html")
+        
+    else:
+        return render_template("register.html")
